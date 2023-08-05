0dc9c60de6b575b8b1f854f68de919582c4c51f5
Sanjeet Yadav <ysanjeet535@gmail.com>
2023-08-05 15:39:25 +0530

    minor

--- src/main/kotlin/leetcode/LeetCode73.kt
+++ src/main/kotlin/leetcode/LeetCode73.kt
@@ -3,7 +3,7 @@ package leetcode
 class LeetCode73 {
     companion object {
         //brute force solution
-        fun setZeroes(matrix: Array<IntArray>): Unit {
+        fun setZeroesBruteForce(matrix: Array<IntArray>): Unit {
             val zeroRows = hashSetOf<Int>()
             val zeroCols = hashSetOf<Int>()
 
@@ -21,10 +21,7 @@ class LeetCode73 {
 
             for(i in 0..(m-1)){
                 for(j in 0..(n-1)){
-                    if(zeroRows.contains(i)){
-                        matrix[i][j] = 0
-                    }
-                    if(zeroCols.contains(j)){
+                    if(zeroRows.contains(i) || zeroCols.contains(j)){
                         matrix[i][j] = 0
                     }
                 }
