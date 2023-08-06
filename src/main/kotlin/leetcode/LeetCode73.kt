1c57ed814e563ea454c1a984abfbb911dcfed710
Sanjeet Yadav <ysanjeet535@gmail.com>
2023-08-06 14:46:22 +0530

    optimal for 73

--- src/main/kotlin/leetcode/LeetCode73.kt
+++ src/main/kotlin/leetcode/LeetCode73.kt
@@ -29,4 +29,53 @@ class LeetCode73 {
 
         }
     }
+
+    //optimal solution , better space
+    fun setZeroes(matrix: Array<IntArray>): Unit {
+
+
+        val m = matrix.size
+        val n = matrix[0].size
+
+        var col0 = -1
+
+        matrix.forEachIndexed { r_i, row  ->
+            row.forEachIndexed {  c_i, e ->
+                if(e == 0){
+                    matrix[r_i][0] = 0
+
+                    if(c_i != 0){
+                        matrix[0][c_i] = 0
+                    } else {
+                        col0 = 0
+                    }
+                }
+            }
+        }
+
+
+
+        for(i in 1..(m-1)){
+            for(j in 1..(n-1)){
+                if(matrix[i][j] != 0){
+                    if(matrix[0][j] == 0 || matrix[i][0] == 0){
+                        matrix[i][j] = 0
+                    }
+                }
+            }
+        }
+
+        if (matrix[0][0] == 0) {
+            for (j in 0..(n-1)) {
+                matrix[0][j] = 0
+            }
+        }
+        if (col0 == 0) {
+            for (i in 0..(m-1)) {
+                matrix[i][0] = 0
+            }
+        }
+
+    }
+
 }
